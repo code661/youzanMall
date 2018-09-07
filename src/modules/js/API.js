3ac8eef9882b79822d3d9636594d6f8ec4f09ba1
Ryan <lucky.tanduzhi@gmail.com>
2018-09-07 15:56:05 +0800

    增加分类页面相关的 url

--- src/modules/js/API.js
+++ src/modules/js/API.js
@@ -2,7 +2,10 @@ let host = 'http://rap2api.taobao.org/app/mock/19448/'
 
 let url = {
   hotList: 'index/hotLists',
-  banner: 'index/banner'
+  banner: 'index/banner',
+  topList: 'category/topList',
+  sublList: 'category/subList',
+  rank: 'category/rank'
 }
 
 for (let key in url){
