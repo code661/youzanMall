d77663549448988a9061cb65344e004115f8fc45
Ryan <lucky.tanduzhi@gmail.com>
2018-09-07 15:50:56 +0800

    修改文字：有赞 --> 首页

--- src/components/bottomNav.vue
+++ src/components/bottomNav.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bottom-nav">
     <ul>
-      <li class="active"><a href="https://maijia.youzan.com/mars/homepage"><i class="icon-home"></i><div>有赞</div></a></li>
+      <li class="active"><a href="https://maijia.youzan.com/mars/homepage"><i class="icon-home"></i><div>首页</div></a></li>
       <li><a href="https://maijia.youzan.com/mars/category"><i class="icon-category"></i><div>分类</div></a></li>
       <li><a href="https://h5.youzan.com/v2/trade/cart?f_platform=yzapp&amp;source=yzapp"><i class="icon-cart"></i><div>购物车</div></a></li>
       <li><a href="https://h5.youzan.com/v2/buyer/member"><i class="icon-user"></i><div>我</div></a></li>
@@ -16,7 +16,7 @@
     height: 50px;
     bottom: 0;
     background-color: #fff;
-    z-index: 8;
+    z-index: 999;
     -o-border-image: url(https://b.yzcdn.cn/v2/image/wap/border-line-2.png) 2
       stretch;
     border-image: url(https://b.yzcdn.cn/v2/image/wap/border-line-2.png) 2 stretch;
