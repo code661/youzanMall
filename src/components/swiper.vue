6b1b490d638368c51f47f1ce5fe34efe49849c5c
Ryan <lucky.tanduzhi@gmail.com>
2018-09-12 02:00:06 +0800

    将 new Swiper 从 watch 数据变化调整到 mouted 生命周期中

--- src/components/swiper.vue
+++ src/components/swiper.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="swiper-container">
     <div class="swiper-wrapper">
-      <div class="swp-page swiper-slide" v-for="(item,index) in banner" key="index">
-        <a class="js-no-follow" :href="item.clickUrl">
+      <div class="swp-page swiper-slide" v-for="(item,index) in banner" :key="index">
+        <a class="imgWrapper" :href="item.clickUrl">
           <img class="goods-main-photo fadeIn" :src="item.img">
         </a>
       </div>
@@ -20,25 +20,35 @@ export default {
     return {};
   },
   props: {
-    banner: {
-      required: true
-    }
+    banner: Array
   },
-  watch: {
-    banner: function(value) {
-      this.$nextTick(() => {
-        var swiper = new Swiper(".swiper-container", {
-          loop: true,
-          pagination: {
-            el: ".swiper-pagination"
-          }
-        });
-      });
-    }
-  }
+  mounted(){
+    var swiper = new Swiper(".swiper-container", {
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination"
+      }
+    })
+  },
+  // 这里遇到了一个小坑
+  // watch: {
+  //   banner: function(value) {
+  //     this.$nextTick(() => {
+  //       var swiper = new Swiper(".swiper-container", {
+  //         loop: true,
+  //         pagination: {
+  //           el: ".swiper-pagination"
+  //         }
+  //       });
+  //     });
+  //   }
+  // }
 };
 </script>
 
 <style scoped>
+.imgWrapper img{
+  width: 100%
+}
 </style>
 
