a9bd8d38a5427a7a1fce3ba959afb35f34f7a918
Ryan <lucky.tanduzhi@gmail.com>
2018-09-16 17:04:33 +0800

    收货地址管理页面，将 HTML 文件写成单 Vue 文件。以通过 Vue-router 来管理路由

--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -1,12 +1,8 @@
-import './assets/member.css'
-import './assets/member_base.css'
-import 'modules/css/common.css'
-import bottomNav from 'components/bottomNav.vue'
-import Vue from 'vue';
+import "modules/css/common.css";
+import Vue from "vue";
+import router from "./router.js"
+
 
 new Vue({
-  el:'.container',
-  components: {
-    'bottom-nav': bottomNav
-  }
-})
+  router,
+}).$mount("#app");
