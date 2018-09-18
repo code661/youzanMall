740e47869c5473e66b5eefc660235dd24cec4184
Ryan <lucky.tanduzhi@gmail.com>
2018-09-18 14:02:41 +0800

    安装 vuex 并注入到根实例中

--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -1,8 +1,9 @@
 import "modules/css/common.css";
 import Vue from "vue";
 import router from "./router/index.js"
-
+import store from "./store/index.js"
 
 new Vue({
   router,
+  store
 }).$mount("#app");
